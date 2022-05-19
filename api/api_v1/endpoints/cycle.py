b46409a1c002690b0b17f4c85a2bfe2ecb5e8af8
Maha Alsaggaf <maha@zyros.com>
2022-05-19 22:15:31 +0300

    implement the worker

--- api/api_v1/endpoints/cycle.py
+++ api/api_v1/endpoints/cycle.py
@@ -1,93 +1,31 @@
-from unittest import result
-from fastapi import APIRouter, Request, Body, Depends, HTTPException
-from fastapi.encoders import jsonable_encoder
-import json
+from fastapi import APIRouter, Request
+from celery_worker import hasPerfectCycles
 from typing import List
-
+from pydantic import BaseModel
 
 router = APIRouter()
 
 
-@router.post("/check", response_model="")
-async def check(request: Request):
+class body(BaseModel):
+   list1: List = []
+
+
+# Kick off a new job by adding it to the work queue
+@router.post("/detectperfectcycles")
+async def addJob(request: Request): 
     lists = await request.json()
+    task = hasPerfectCycles.delay(lists)     
+    return {"Message": "Thank you for submitting your data", 
+            "Task ID": task.id}
 
-    for list in lists:
-        visitedNodes = []
-
-        print("***** New List *****")
-        print(lists[list])
-        lengthOfTheList = len(lists[list])
-
-        print("lengthOfTheList =", lengthOfTheList)
-
-        if(lengthOfTheList == 0):
-            lists[list] = False
-            print("len of the list = 0")
-        elif(lengthOfTheList == 1):
-            if(lists[list][0] == 0):
-                lists[list] = True
-                print("len of the list = 1")
-            else:
-                lists[list] = False
-        else:
-            # print("len of the list is large")
-            # print(lengthOfTheList)
-            fastPointer = lists[list][1] + 0
-            slowPointer = lists[list][0] + 0
-
-          
-            print("fastPointer 1 = ", fastPointer)
-            print("slowPointer 0 = ", slowPointer)
-
-            itration = 0
-            while(fastPointer < lengthOfTheList):
-                itration = itration + 1
-
-                print("##### itration ######")
-                print("value of the slow pointer = ", slowPointer,
-                      " = ", lists[list][slowPointer])
-                print("value of the fast pointer = ", fastPointer,
-                      " = ", lists[list][fastPointer])
-
-                if(itration == 20):
-                    break
-                pointingNode = slowPointer
-                pointedNode = lists[list][slowPointer]
-                # TODO improve this
-                if pointingNode not in visitedNodes:
-                    visitedNodes.append(pointingNode)
-                if pointedNode not in visitedNodes:
-                    visitedNodes.append(pointedNode)
-
-                if(lists[list][fastPointer] == lists[list][slowPointer]):
-                    print("there is a cycle")
-
-                    lastVisitedElementPointer = lists[list][slowPointer]
-                    lastVisitedElement = lists[list][lastVisitedElementPointer] + 0
-
-                    if(lastVisitedElement == 0):
-                        print(
-                            "The last element to be visited takes you back to position zero")
-                        print("back to the beginning of the list")
-                        lists[list] = True
-                    break
-
-                if(fastPointer < lengthOfTheList-1):
-                    fastPointer = lists[list][lists[list][fastPointer]]
-                else:
-                    fastPointer = lists[list][0]
-
-                if(slowPointer < lengthOfTheList):
-                    slowPointer = lists[list][slowPointer]
-                else:
-                    slowPointer = lists[list][0]
-
-            if(lists[list] != True):
-                lists[list] = False
-            else:
-                # All elements of the list must be visited.
-                if(len(visitedNodes) != lengthOfTheList-1):
-                    lists[list] = False
-
-    return lists
+# Allows the client to query the state of a background job
+@router.get("/detectperfectcycles/{task_id}")
+def get_status(task_id):
+    task_result = hasPerfectCycles.AsyncResult(task_id)
+    print(task_result)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return result
---
+++ celery_worker.py
@@ -0,0 +1,85 @@
+from celery import Celery
+from celery.utils.log import get_task_logger
+
+# Initialize celery
+celery = Celery('tasks', backend='redis://' ,broker='amqp://guest:guest@127.0.0.1:5672//')
+celery.conf.CELERY_IGNORE_RESULT = False
+celery.conf.CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+
+@celery.task
+def hasPerfectCycles(lists):
+    for list in lists:
+        hasPerfectCycle = False
+        visitedNodes = lists[list][:]
+        lengthOfTheList = len(lists[list])
+
+
+        if(lengthOfTheList == 0):
+            lists[list] = False
+        elif(lengthOfTheList == 1):
+            if(lists[list][0] == 0):
+                lists[list] = True
+            else:
+                lists[list] = False
+        else:
+            fastPointer = lists[list][1] 
+            slowPointer = lists[list][0] 
+
+            beginningOfTheListElement = lists[list][0] 
+
+
+            while(fastPointer < lengthOfTheList):
+                
+                if(lists[list][fastPointer] == lists[list][slowPointer]):
+                    hasPerfectCycle = True
+                    break
+
+                if(fastPointer < lengthOfTheList-1):
+                    fastPointer = lists[list][lists[list][fastPointer]]
+                else:
+                    fastPointer = lists[list][0]
+
+                if(slowPointer < lengthOfTheList):
+                    slowPointer = lists[list][slowPointer]
+                else:
+                    slowPointer = lists[list][0]
+
+            if(hasPerfectCycle != True):
+                hasPerfectCycle = False
+            else:
+                traverse = lists[list][0]
+                visitedNodes[0] = True
+                for x in range(lengthOfTheList):
+                    traverse = lists[list][traverse]
+                    visitedNodes[traverse] = True
+
+                #The last element to be visited takes you back to position zero
+                if(traverse != beginningOfTheListElement):
+                   hasPerfectCycle = False
+                
+                # All elements of the list must be visited.
+                for node in visitedNodes: 
+                    if(type(node) != type(True)): 
+                        hasPerfectCycle = False
+                        break; 
+
+                if(hasPerfectCycle):
+                    lists[list] = True
+                else:
+                    lists[list] = False
+
+
+
+# lastVisitedElementPointer = lists[list][slowPointer]
+#                     print("lastVisitedElementPointer = ", lastVisitedElementPointer)
+#                     lastVisitedElement = lists[list][lastVisitedElementPointer] 
+#                     print("lastVisitedElement = ", lastVisitedElement)
+
+#                     visitedNodes[lastVisitedElementPointer] = True
+
+#                     if( lastVisitedElementPointer == beginningOfTheListElement):
+#                         print("The last element to be visited takes you back to position zero")
+#                         print("back to the beginning of the list")
+#                         lists[list] = True
+    return lists
